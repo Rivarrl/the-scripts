35d594a84f37337cbe049a8e596a9b2f5e41670b
Rivarrl <1049793871@qq.com>
2020-02-07 22:28:29 +0800

    update: manga.py

--- sp/manga.py
+++ sp/manga.py
@@ -7,7 +7,7 @@
 import os
 import requests
 from bs4 import BeautifulSoup
-
+import pickle
 # 漫画爬虫
 
 # 保存路径
@@ -15,32 +15,60 @@ dest_path = "D:/data/manga"
 # 记录一些漫画网址
 urls = {}
 # 漫画铺
-urls['mhp'] = "https://www.manhuapu.com/"
+urls['mhp'] = "https://www.manhuapu.com"
 
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
 }
+html_parser = 'html.parser'
 
-def mhp_crawl(suffix):
+def mhp_crawl_menu(suffix):
     folder_name = suffix.strip('/').split('/')[-1]
     _path = os.path.join(dest_path, folder_name)
     if not os.path.exists(_path):
         os.mkdir(_path)
-    t = '1.html'
-    if os.path.exists(t):
-        with open(t, 'r', encoding='utf-8') as f:
+    _pkl_path = os.path.join(_path, 'menu.pkl')
+    if os.path.exists(_pkl_path):
+        return _pkl_path
+    _html_path = os.path.join(_path, 'menu.html')
+    if os.path.exists(_html_path):
+        with open(_html_path, 'r', encoding='utf-8') as f:
             resp_text = f.read()
     else:
-        _url = urls['mhp'] + suffix
+        _url = urls['mhp'] + '/' + suffix
         resp = requests.get(_url, headers)
         resp.encoding = resp.apparent_encoding
         resp_text = resp.text
-        with open(t, 'w', encoding='utf-8') as f:
+        with open(_html_path, 'w', encoding='utf-8') as f:
             f.write(resp_text)
-    bf = BeautifulSoup(resp_text, 'html.parser')
-    links_li = bf.select('#play_0 > ul > li')
-    for li in links_li:
-        print(li)
+    bf = BeautifulSoup(resp_text, html_parser)
+    links_a = bf.select('#play_0 > ul > li > a')
+    pkd = {}
+    for a in links_a:
+        p_name = a.get_text()
+        p_href = a.get('href')
+        pkd[p_name] = p_href
+    with open(_pkl_path, 'wb') as f:
+        pickle.dump(pkd, f)
+    return _pkl_path
+
+
+def mhp_crawl(suffix):
+    _pkl_path = mhp_crawl_menu(suffix)
+    with open(_pkl_path, 'rb') as f:
+        pkd = pickle.load(f)
+    if not pkd: return
+    for p_name, p_href in pkd.items():
+        print(p_name, p_href)
+        _url = urls['mhp'] + p_href
+        _url_p = lambda x: _url + '?p={}'.format(x)
+        resp_1 = requests.get(_url, headers)
+        resp_1.encoding = resp_1.apparent_encoding
+        bf_1 = BeautifulSoup(resp_1.text, html_parser)
+        options = bf_1.select('#k_pageSelect')
+        pages = options[-1].get('value')
+        print(pages)
+        break
 
 if __name__ == '__main__':
     # 声之形
